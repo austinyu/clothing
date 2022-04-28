87124ea9188a267b958447e3c33e082263bde097
Austin <yuxm.austin2016@outlook.com>
2022-04-28 12:39:59 -0500

    firebase google signin added

--- clothing/src/App.js
+++ clothing/src/App.js
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.componet";
 import Navigation from "./routes/navigation/navigation.component";
-
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />} >
         <Route index element={<Home />}/>  
+        <Route path="sign-in" element={<SignIn />}/>  
         {/* <Route path="shop" element={<Shop />} /> */}
       </ Route>
     </Routes>
